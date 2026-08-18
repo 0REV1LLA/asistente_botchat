58e7f8a400ea11f373b51a35b1dfb9305e8031d9
Luis Revilla <revillarys@gmail.com>
2026-08-17 22:19:53 -0400

    Refactor vercel-start.sh to improve deployment preparation steps

--- vercel-start.sh
+++ vercel-start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Instala dependencias y prepara la app para despliegue en Vercel
+
+python -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
